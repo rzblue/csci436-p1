11d3d3c9ca72df734b294bc1c244f4606b15d0af
Kyle Horn <hornk@purdue.edu>
2025-10-24 11:12:15 -0400

    fixed spelling error in Logger.hpp on line 9

--- include/Logger.hpp
+++ include/Logger.hpp
@@ -6,7 +6,7 @@ class Logger {
     public:
         Logger(std::string clientID);
         ~Logger();
-        void logRequest(std::string rquest); //Log the request and timestamp
+        void logRequest(std::string request); //Log the request and timestamp
         void logResponse(std::string response); //Log the response and timestamp
     private:
         std::string clientID;
